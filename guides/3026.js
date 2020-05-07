63d6222af1d9230dd9bec273c8db012ccc732450
Dmitry Shin <dmitry.s@hsdn.org>
2020-05-07 08:30:18 +0400

    Update

--- guides/3026.js
+++ guides/3026.js
@@ -2,7 +2,7 @@
 //
 // made by michengs
 
-const {MARKER_ITEM, SpawnVector, SpawnCircle, SpawnSemicircle} = require("../lib");
+const {MARKER_ITEM, SpawnMarker, SpawnVector, SpawnCircle, SpawnSemicircle} = require("../lib");
 
 let player, entity, library, effect;
 let debuff = null;
@@ -11,6 +11,8 @@ let timer2;
 let timer3;
 let timer4;
 let timer5;
+let boss_entity;
+let boss_offset = 0;
 let qbacting = null;
 let blue = false;
 let red = false;
@@ -25,6 +27,20 @@ const debuff_TipMsg =
 	0: {msgt: 'Ready to get Fire debuff', msg: 'Готовность к переключению на Огонь'},
 	1: {msgt: 'Ready to get Ice debuff',  msg: 'Готовность к переключению на Лед'}
 };
+
+function spawn_marker(out, handlers) {
+	if (!boss_entity) return;
+	let distance = 220;
+	let caption = "IN";
+	if (out) {
+		distance = 620;
+		caption = "OUT";
+	}
+	SpawnMarker(false,  45 + boss_offset, distance, 0, 4000, true, [caption, "SAFE"], handlers, null, boss_entity);
+	SpawnMarker(false, 135 + boss_offset, distance, 0, 4000, true, [caption, "SAFE"], handlers, null, boss_entity);
+	SpawnMarker(false, 225 + boss_offset, distance, 0, 4000, true, [caption, "SAFE"], handlers, null, boss_entity);
+	SpawnMarker(false, 315 + boss_offset, distance, 0, 4000, true, [caption, "SAFE"], handlers, null, boss_entity);
+}
 
 function skilld_event(skillid, handlers, event, entity, dispatch) {
 	if ([3026004,3126004,3026005,3126005].includes(skillid)) {   // ярость 0, ужас 1
@@ -90,14 +106,17 @@ function skilld_event(skillid, handlers, event, entity, dispatch) {
 	}
 	// In-Out identification
 	if ([212,213,214,215].includes(skillid)) {
+		boss_entity = entity;
 		SpawnCircle(false,445,0,0,8,440,200,8000,handlers,event,entity);
 		SpawnCircle(false,445,0,0,4,840,200,8000,handlers,event,entity);
 	}
 	if ([212,214].includes(skillid)) {   // Fire claw (141, 142)
+		boss_offset = 10;
 		SpawnVector(553,0,0,190,840,200,8000,handlers,event,entity);
 		SpawnVector(553,0,0, 10,840,200,8000,handlers,event,entity);
 	}
 	if ([213,215].includes(skillid)) {   // Ice claw (143, 144)
+		boss_offset = -10;
 		SpawnVector(553,0,0,170,840,200,8000,handlers,event,entity);
 		SpawnVector(553,0,0,350,840,200,8000,handlers,event,entity);
 	}
@@ -108,6 +127,7 @@ function skilld_event(skillid, handlers, event, entity, dispatch) {
 				"message": (`Ice inside | ${CK_TipMsg[(qbacting + debuff + 1) % 2].msgt}`),
 				"message_RU": (`Внутри лед | ${CK_TipMsg[(qbacting + debuff + 1) % 2].msg}`)
 			});
+			spawn_marker((qbacting + debuff + 1) % 2, handlers);
 			blue = true;
 			red  = false;
 			setTimeout(() => blue = false, 6500); //6700
@@ -120,6 +140,7 @@ function skilld_event(skillid, handlers, event, entity, dispatch) {
 				"message": (`Fire inside | ${CK_TipMsg[(qbacting + debuff) % 2].msgt}`),
 				"message_RU": (`Внутри огонь | ${CK_TipMsg[(qbacting + debuff) % 2].msg}`)
 			});
+			spawn_marker((qbacting + debuff) % 2, handlers);
 			blue = false;
 			red  = true;
 			setTimeout(() => red = false, 6500);
@@ -143,16 +164,18 @@ function start_debuff(handlers, event, entity, dispatch) {
 				debuff = event.id;
 				if (blue) {
 					handlers['text']({
-						"sub_type": "notification",
+						"sub_type": "message",
 						"message": (`${CK_TipMsg[(qbacting + debuff + 1) % 2].msgt}`),
 						"message_RU": (`${CK_TipMsg[(qbacting + debuff + 1) % 2].msg}`)
 					});
+					spawn_marker((qbacting + debuff + 1) % 2, handlers);
 				} else if (red) {
 					handlers['text']({
-						"sub_type": "notification",
+						"sub_type": "message",
 						"message": (`${CK_TipMsg[(qbacting + debuff) % 2].msgt}`),
 						"message_RU": (`${CK_TipMsg[(qbacting + debuff) % 2].msg}`)
 					});
+					spawn_marker((qbacting + debuff) % 2, handlers);
 				}
 			} else {
 				debuff = null;
