ef4c6a1032a6169b0c3c7a65eea5099a5135411b
Dmitry Shin <dmitry.s@hsdn.org>
2020-08-09 01:56:29 +0400

    Update

--- voice/index.js
+++ voice/index.js
@@ -5,6 +5,6 @@ var path = require('path');
 var voice = exports;
 const { exec } = require('child_process');
 voice.speak = function(message,rate) {
-	message = message.replace(/<-/g, '|').replace(/->/g, '|').replace(/</g, '|').replace(/>/g, '|');
+	message = message.replace(/<-/g, '|').replace(/->/g, '|').replace(/</g, '|').replace(/>/g, '|').toLowerCase();
 	exec(`powershell.exe Add-Type -AssemblyName System.speech; $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; $speak.Rate = ${rate}; [Console]::InputEncoding = [System.Text.Encoding]::UTF8; $speak.Speak([Console]::In.ReadToEnd()); exit`).stdin.end(message);
 };
