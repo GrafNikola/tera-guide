e01ceb9ded134540f8442693217d7fb975403e52
Dmitry Shin <dmitry.s@hsdn.org>
2020-08-28 01:07:29 +0400

    Update

--- guides/3103.js
+++ guides/3103.js
@@ -11,32 +11,35 @@ module.exports = {
 		({ player, entity, library, effect } = dispatch.require.library);
 	},
 
-	// 775
-	//"s-3103-1000-101-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "_","message_RU": "101 удар вперед"}],
-	//"s-3103-1000-102-0": [{"type": "text","sub_type": "message","message": "_","message_RU": "102 разворот"}],
-	//"s-3103-1000-107-0": [{"type": "text","sub_type": "message","message": "_","message_RU": "107"}], // 08-16
-	//"s-3103-1000-108-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "_","message_RU": "108 полукруг вперед"}],
-	//"s-3103-1000-111-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "_","message_RU": "111 круг вперед"}], // 07-40
-	//"s-3103-1000-113-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "_","message_RU": "113 удар ногой | кувырок"}], // 113 121
+	//"s-3103-1000-101-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Punch","message_RU": "Серия ударов"}],
+	//"s-3103-1000-102-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Turn around","message_RU": "Разворот"}],
+	"s-3103-1000-108-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Floor punch","message_RU": "Удар о землю"}],
+	//"s-3103-1000-113-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Roundhouse kick","message_RU": "Удар с разворота"}],
+	//"s-3103-1000-121-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Flip kick","message_RU": "Удар в воздухе"}],
+	//"s-3103-1000-154-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Jumping kick","message_RU": "Удар в прыжке"}],
+	"s-3103-1000-111-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Knockdown","message_RU": "Опрокид"}],
+	"s-3103-1000-115-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Front kick","message_RU": "Удар вперед"}],
+
 	//"s-3103-1000-120-0": [{"type": "text","sub_type": "message","message": "_","message_RU": "120"}], // 08-01
-	//"s-3103-1000-121-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "_","message_RU": "121 кувырок"}], // 07-52
 	//"s-3103-1000-124-0": [{"type": "text","sub_type": "message","message": "_","message_RU": "124"}], // 07-34
 	//"s-3103-1000-125-0": [{"type": "text","sub_type": "message","message": "_","message_RU": "125"}], // 07-25
 	//"s-3103-1000-127-0": [{"type": "text","sub_type": "message","message": "_","message_RU": "127"}], // 07-22
-	//"s-3103-1000-153-0": [{"type": "text","sub_type": "message","message": "_","message_RU": "153 удар левой правой"}], // 07-26
-	//"s-3103-1000-154-0": [{"type": "text","sub_type": "message","message": "_","message_RU": "154 удар в прыжке"}], // 08-30
+	//"s-3103-1000-153-0": [{"type": "text","sub_type": "message","message": "_","message_RU": "153"}], // 02-32
 
+	"s-3103-1000-107-0": [{"type": "text","sub_type": "message","message": "Target (Bait)","message_RU": "Байт (таргет)"}],
 	"s-3103-1000-110-0": [{"type": "text","sub_type": "message","message": "Spin","message_RU": "Крутилка"},{"type": "func","func": SpawnCircle.bind(null,true,553,0,0,12,420,0,3000)}],
 	"s-3103-1000-114-0": [{"type": "text","sub_type": "message","message": "Jump (Knockdown)","message_RU": "Прыжок (опрокид)"},{"type": "func","func": SpawnCircle.bind(null,true,553,0,0,12,230,0,2000)}],
-	"s-3103-1000-115-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Front","message_RU": "Удар вперед"}],
-	"s-3103-1000-116-0": [{"type": "text","sub_type": "message","message": "Target (Bait)","message_RU": "Удар (таргет)"}],
-	"s-3103-1000-146-0": [{"type": "text","sub_type": "message","message": "Back kick","message_RU": "Откид назад"},
+	"s-3103-1000-116-0": [{"type": "text","sub_type": "message","message": "Kick","message_RU": "Удар"}],
+	"s-3103-1000-146-0": [{"type": "text","sub_type": "message","message": "Back kick","message_RU": "Откид назад"}, // 116 146
 		{"type": "func","func": SpawnVector.bind(null,553,90,120,170,600,0,3000)},
 		{"type": "func","func": SpawnVector.bind(null,553,270,120,-170,600,0,3000)}
 	],
 
 	// Shield
 	"qb-3103-1000-31031006": [{"type": "text","sub_type": "message","message": "SHIELD!","message_RU": "ЩИТ!"}],
+
+	// Target 31031007 308
+	"qb-3103-1000-31031007": [{"type": "text","sub_type": "message","message": "Target","message_RU": "Таргет"}],
 
 	// Donuts
 	"qb-3103-1000-31031008": [{"type": "text","sub_type": "message","message": "Donuts: Out > In > Dodge","message_RU": "Бублики: От него > К нему > Эвейд"}], // 31031008 303/304 117 155
@@ -51,6 +54,8 @@ module.exports = {
 		{"type": "func","func": SpawnCircle.bind(null,false,445,0,0,12,250,0,5000)},
 		{"type": "func","func": SpawnCircle.bind(null,false,445,0,0,8,480,0,5000)}
 	],
+	"s-3103-1000-155-0": [{"type": "text","sub_type": "message","delay": 500,"message": "Dodge","message_RU": "Эвейд"}],
+		
 
 	// Stun 142 148 129
 	"s-3103-1000-142-0": [{"type": "text","sub_type": "message","message": "Stun | Back wave","message_RU": "Стан | Волна назад"}],
@@ -63,9 +68,6 @@ module.exports = {
 	// Jump 143-0 143-1
 	"s-3103-1000-143-0": [{"type": "text","sub_type": "message","message": "Jump (Stun)","message_RU": "Прыжок (стан)"}],
 	"s-3103-1000-143-1": [{"type": "func","func": SpawnCircle.bind(null,true,553,0,0,14,230,0,2000)}],
-
-	// Target 31031007 308
-	"qb-3103-1000-31031007": [{"type": "text","sub_type": "message","message": "Target","message_RU": "Таргет"}],
 
 	// AoE 313 314
 	"s-3103-1000-313-0": [{"type": "text","sub_type": "message","message": "AOE","message_RU": "AOE"}],
--- guides/3203.js
+++ guides/3203.js
@@ -33,17 +33,29 @@ module.exports = {
 	"h-3203-1000-99": [{"type": "func","func": start_boss}],
 	"h-3203-1000-30": [{"type": "func","func": print_thirty}],
 
+	//"s-3203-1000-101-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Punch","message_RU": "Серия ударов"}],
+	//"s-3203-1000-102-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Turn around","message_RU": "Разворот"}],
+	"s-3203-1000-108-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Floor punch","message_RU": "Удар о землю"}],
+	//"s-3203-1000-113-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Roundhouse kick","message_RU": "Удар с разворота"}],
+	//"s-3203-1000-121-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Flip kick","message_RU": "Удар в воздухе"}],
+	//"s-3203-1000-154-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Jumping kick","message_RU": "Удар в прыжке"}],
+	"s-3203-1000-111-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Knockdown","message_RU": "Опрокид"}],
+	"s-3203-1000-115-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Front kick","message_RU": "Удар вперед"}],
+
+	"s-3203-1000-107-0": [{"type": "text","sub_type": "message","message": "Target (Bait)","message_RU": "Байт (таргет)"}],
 	"s-3203-1000-110-0": [{"type": "text","sub_type": "message","message": "Spin","message_RU": "Крутилка"},{"type": "func","func": SpawnCircle.bind(null,true,553,0,0,12,420,0,3000)}],
 	"s-3203-1000-114-0": [{"type": "text","sub_type": "message","message": "Jump (Knockdown)","message_RU": "Прыжок (опрокид)"},{"type": "func","func": SpawnCircle.bind(null,true,553,0,0,12,230,0,2000)}],
-	"s-3203-1000-115-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Front","message_RU": "Удар вперед"}],
-	"s-3203-1000-116-0": [{"type": "text","sub_type": "message","message": "Target (Bait)","message_RU": "Удар (таргет)"}],
-	"s-3203-1000-146-0": [{"type": "text","sub_type": "message","message": "Back kick","message_RU": "Откид назад"},
+	"s-3203-1000-116-0": [{"type": "text","sub_type": "message","message": "Kick","message_RU": "Удар"}],
+	"s-3203-1000-146-0": [{"type": "text","sub_type": "message","message": "Back kick","message_RU": "Откид назад"}, // 116 146
 		{"type": "func","func": SpawnVector.bind(null,553,90,120,170,600,0,3000)},
 		{"type": "func","func": SpawnVector.bind(null,553,270,120,-170,600,0,3000)}
 	],
 
 	// Shield
 	"qb-3203-1000-32031006": [{"type": "text","sub_type": "message","message": "SHIELD!","message_RU": "ЩИТ!"}],
+
+	// Target 32031007 308
+	"qb-3203-1000-32031007": [{"type": "text","sub_type": "message","message": "Target","message_RU": "Таргет"}],
 
 	// Donuts
 	"qb-3203-1000-32031008": [{"type": "text","sub_type": "message","message": "Donuts: Out > In > Dodge","message_RU": "Бублики: От него > К нему > Эвейд"}], // 32031008 303/304 117 155
@@ -58,6 +70,7 @@ module.exports = {
 		{"type": "func","func": SpawnCircle.bind(null,false,445,0,0,12,250,0,5000)},
 		{"type": "func","func": SpawnCircle.bind(null,false,445,0,0,8,480,0,5000)}
 	],
+	"s-3203-1000-155-0": [{"type": "text","sub_type": "message","delay": 500,"message": "Dodge","message_RU": "Эвейд"}],
 
 	// Stun 142 148 129
 	"s-3203-1000-142-0": [{"type": "text","sub_type": "message","message": "Stun | Back wave","message_RU": "Стан | Волна назад"}],
@@ -70,9 +83,6 @@ module.exports = {
 	// Jump 143-0 143-1
 	"s-3203-1000-143-0": [{"type": "text","sub_type": "message","message": "Jump (Stun)","message_RU": "Прыжок (стан)"}],
 	"s-3203-1000-143-1": [{"type": "func","func": SpawnCircle.bind(null,true,553,0,0,14,230,0,2000)}],
-
-	// Target 32031007 308
-	"qb-3203-1000-32031007": [{"type": "text","sub_type": "message","message": "Target","message_RU": "Таргет"}],
 
 	// AoE 313 314
 	"s-3203-1000-313-0": [{"type": "text","sub_type": "message","message": "AOE","message_RU": "AOE"}],
