92e87cb1c5d247394ed17e3c68d617e85f402ca5
Dmitry Shin <dmitry.s@hsdn.org>
2020-08-29 16:38:52 +0400

    Update

--- guides/3103.js
+++ guides/3103.js
@@ -13,24 +13,21 @@ module.exports = {
 
 	//"s-3103-1000-101-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Punch","message_RU": "Серия ударов"}],
 	"s-3103-1000-113-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Roundhouse kick","message_RU": "Удар с разворота"}],
+	//"s-3103-1000-120-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Knockdown","message_RU": "Опрокид"}], // 08-01
 	"s-3103-1000-121-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Flip kick (Stun)","message_RU": "Удар в воздухе (стан)"}],
 	"s-3103-1000-111-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Knockdown","message_RU": "Опрокид"}],
 	"s-3103-1000-102-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Combo","message_RU": "Комба"}], // 102 153/154 115/116
 	"s-3103-1000-115-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Front kick","message_RU": "Удар вперед"}],
 	"s-3103-1000-153-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Two kicks","message_RU": "Два удара"}], // 153 108
 	//"s-3103-1000-108-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Floor punch","message_RU": "Удар о землю"}],
-
-	//"s-3103-1000-120-0": [{"type": "text","sub_type": "message","message": "_","message_RU": "120"}], // 08-01
-	//"s-3103-1000-124-0": [{"type": "text","sub_type": "message","message": "_","message_RU": "124"}], // 07-34
-	//"s-3103-1000-125-0": [{"type": "text","sub_type": "message","message": "_","message_RU": "125"}], // 07-25
-	//"s-3103-1000-127-0": [{"type": "text","sub_type": "message","message": "_","message_RU": "127"}], // 07-22
+	//"s-3103-1000-127-0": [{"type": "text","class_position":"tank","sub_type": "message","message": "Many kicks","message_RU": "Несколько ударов"}],
 
 	"s-3103-1000-107-0": [{"type": "text","sub_type": "message","message": "Target (Bait)","message_RU": "Байт (таргет)"}],
 	"s-3103-1000-110-0": [{"type": "text","sub_type": "message","message": "Spin","message_RU": "Крутилка"},{"type": "func","func": SpawnCircle.bind(null,true,553,0,0,12,420,0,3000)}],
-	"s-3103-1000-114-0": [{"type": "text","sub_type": "message","message": "Jump (Knockdown)","message_RU": "Прыжок (опрокид)"},{"type": "func","func": SpawnCircle.bind(null,true,553,0,0,12,240,0,2000)}],
+	"s-3103-1000-114-0": [{"type": "text","sub_type": "message","message": "Leap (Knockdown)","message_RU": "Прыжок (опрокид)"},{"type": "func","func": SpawnCircle.bind(null,true,553,0,0,12,240,0,2000)}],
 	//"s-3103-1000-154-0": [{"type": "text","sub_type": "message","message": "Jumping kick","message_RU": "Удар в прыжке"}], // 154 310 116
 	"s-3103-1000-310-0": [{"type": "text","sub_type": "message","message": "Back flip | Kick","message_RU": "Сальто назад | Удар"}], // 310 116
-	"s-3103-1000-116-0": [{"type": "text","sub_type": "message","message": "Kick","message_RU": "Удар"}],
+	"s-3103-1000-116-0": [{"type": "text","sub_type": "message","message": "Kick","message_RU": "Мощный удар"}],
 	"s-3103-1000-131-0": [{"type": "text","sub_type": "message","message": "Rhythmic Blows","message_RU": "Ураганная серия"}], // 131 132 133
 	"s-3103-1000-146-0": [{"type": "text","sub_type": "message","message": "Back kick","message_RU": "Откид назад"}, // 116 146
 		{"type": "func","func": SpawnVector.bind(null,553,90,120,170,600,0,3000)},
@@ -40,8 +37,10 @@ module.exports = {
 	// Shield
 	"qb-3103-1000-31031006": [{"type": "text","sub_type": "message","message": "SHIELD!","message_RU": "ЩИТ!"}],
 
-	// Target 31031007 308
+	// Target 308 31031007 125
 	"qb-3103-1000-31031007": [{"type": "text","sub_type": "message","message": "Target","message_RU": "Таргет"}],
+	"s-3103-1000-124-0": [{"type": "text","sub_type": "message","message": "Kick","message_RU": "Удар ногой"}], // 305 124
+	"s-3103-1000-125-0": [{"type": "text","sub_type": "message","message": "Kick","message_RU": "Удар ногой"}],
 
 	// Donuts
 	"qb-3103-1000-31031008": [{"type": "text","sub_type": "message","message": "Donuts: Out > In > Dodge","message_RU": "Бублики: От него > К нему > Эвейд"}], // 31031008 303/304 117 155
