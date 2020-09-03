d7d23dbb384464505b73f68730b209ed1fee1361
Dmitry Shin <dmitry.s@hsdn.org>
2020-09-03 16:29:26 +0400

    Update

--- dispatch.js
+++ dispatch.js
@@ -1,47 +1,47 @@
 class DispatchWrapper {
-    constructor(dispatch) {
-        this._dispatch = dispatch;
-        this._hooks = [];
-        this._timers = {};
+	constructor(dispatch) {
+		this._dispatch = dispatch;
+		this._hooks = [];
+		this._timers = {};
 		this._random_timer_id = 0xFFFFFFFA;
-    }
+	}
 
-    hook(...args) {
-        this._hooks.push(this._dispatch.hook(...args));
-    }
+	hook(...args) {
+		this._hooks.push(this._dispatch.hook(...args));
+	}
 
-    hookOnce(...args) {
-        this._dispatch.hookOnce(...args);
-    }
+	hookOnce(...args) {
+		this._dispatch.hookOnce(...args);
+	}
 
-    unhook(...args) {
-        throw new Error("unhook not supported for tera-guide");
-    }
+	unhook(...args) {
+		throw new Error("unhook not supported for tera-guide");
+	}
 
-    _remove_all_hooks() {
-        for(const hook of this._hooks) this._dispatch.unhook(hook);
-    }
+	_remove_all_hooks() {
+		for(const hook of this._hooks) this._dispatch.unhook(hook);
+	}
 
-    get require() {
-        return this._dispatch.require;
-    }
+	get require() {
+		return this._dispatch.require;
+	}
 
-    setTimeout(...args) {
-        return this._timers[--this._random_timer_id] = this._dispatch.setTimeout(...args);
-    }
+	setTimeout(...args) {
+		return this._timers[--this._random_timer_id] = this._dispatch.setTimeout(...args);
+	}
 
-    clearTimeout(...args) {
-        return this._dispatch.clearTimeout(...args);
-    }
+	clearTimeout(...args) {
+		return this._dispatch.clearTimeout(...args);
+	}
 
-    _clear_all_timers() {
-        for(const key in this._timers) this.clearTimeout(this._timers[key]);
-        this._timers = {};
-    }
+	_clear_all_timers() {
+		for(const key in this._timers) this.clearTimeout(this._timers[key]);
+		this._timers = {};
+	}
 
-    toServer(...args) { return this.send(...args); }
-    toClient(...args) { return this.send(...args); }
-    send(...args) { return this._dispatch.send(...args); }
+	toServer(...args) { return this.send(...args); }
+	toClient(...args) { return this.send(...args); }
+	send(...args) { return this._dispatch.send(...args); }
 }
 
 module.exports = DispatchWrapper;
