5d96eb2c0f4666aa2433c0cc652e2bc61f090f98
Dmitry Shin <dmitry.s@hsdn.org>
2020-09-05 17:24:54 +0400

    Update

--- voice/index.js
+++ voice/index.js
@@ -1,10 +1,33 @@
 'use strict';
 
-var child_process = require('child_process');
-var path = require('path');
-var voice = exports;
 const { exec } = require('child_process');
-voice.speak = function(message,rate) {
-	message = message.replace(/<-/g, '|').replace(/->/g, '|').replace(/</g, '|').replace(/>/g, '|').toLowerCase();
-	exec(`powershell.exe Add-Type -AssemblyName System.speech; $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; $speak.Rate = ${rate}; [Console]::InputEncoding = [System.Text.Encoding]::UTF8; $speak.Speak([Console]::In.ReadToEnd()); exit`).stdin.end(message);
+
+function format_message(message) {
+	const unescape_map = {
+		'&amp;': '&',
+		'&lt;': '<',
+		'&gt;': '>',
+		'&quot;': '"',
+		'&#039;': "'"
+	};
+	message = message.toLowerCase();
+	for (const [key, value] of Object.entries(unescape_map)) {
+		message = message.replace(key, value);
+	}
+	for (const value of ['<-','->','<','>']) {
+		message = message.replace(value, ' ');
+	}
+	return message;
+}
+
+exports.speak = function(message, rate) {
+	const command =
+		'powershell.exe Add-Type -AssemblyName System.speech; ' +
+		'$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; ' +
+		'$speak.Rate = ' + rate + '; ' +
+		'[Console]::InputEncoding = [System.Text.Encoding]::UTF8; ' +
+		'$speak.Speak([Console]::In.ReadToEnd()); ' + 
+		'exit'
+	;
+	exec(command).stdin.end(format_message(message));
 };
