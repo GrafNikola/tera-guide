ac70be96269ae345be588472043f676392307579
Dmitry Shin <dmitry.s@hsdn.org>
2020-12-13 21:09:08 +0400

    Update

--- guides/9056.js
+++ guides/9056.js
@@ -9,18 +9,24 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		// Boss 1 (phase 1)
 		"s-456-401-104-0": [
 			{ type: "text", sub_type: "message", message: "Bomb", message_RU: "Бомба" },
+			{ type: "text", sub_type: "warning", message: "(1)", speech: false, delay: 300 },
+			{ type: "text", sub_type: "warning", message: "(2)", speech: false, delay: 800 },
+			{ type: "text", sub_type: "warning", message: "(3)", speech: false, delay: 1300 },
+			{ type: "text", sub_type: "message", message: "(4) Dodge!", message_RU: "(4) Эвейд!", delay: 1800 }
+		],
+		"s-456-1001-107-0": [{ type: "text", sub_type: "alert", message: "Shot", message_RU: "Выстрел", speech: false }],
+
+		// Boss 1 (phase 2)
+		"s-456-413-104-0": [
+			{ type: "text", sub_type: "message", message: "Bomb", message_RU: "Бомба" },
 			{ type: "text", sub_type: "warning", message: "(1)", speech: false, delay: 550 },
 			{ type: "text", sub_type: "warning", message: "(2)", speech: false, delay: 1300 },
 			{ type: "text", sub_type: "warning", message: "(3)", speech: false, delay: 2050 },
 			{ type: "text", sub_type: "message", message: "(4) Dodge!", message_RU: "(4) Эвейд!", delay: 2800 }
 		],
-		"s-456-1001-107-0": [{ type: "text", sub_type: "alert", message: "Shot", message_RU: "Выстрел", speech: false }],
-
-		// Boss 1 (phase 2)
-		"s-456-413-104-0": "s-456-401-104-0",
-		"s-456-414-104-0": "s-456-401-104-0",
-		"s-456-415-104-0": "s-456-401-104-0",
-		"s-456-416-104-0": "s-456-401-104-0",
+		"s-456-414-104-0": "s-456-413-104-0",
+		"s-456-415-104-0": "s-456-413-104-0",
+		"s-456-416-104-0": "s-456-413-104-0",
 		"s-456-1000-107-0": [{ type: "text", sub_type: "alert", message: "Shot", message_RU: "Выстрел", speech: false }],
 		"ab-456-1000-905685": [
 			{ type: "text", sub_type: "message", message: "Plague of Exhaustion", message_RU: "Регресс", class_position: "priest" },
@@ -33,6 +39,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-456-1002-104-0": [{ type: "text", sub_type: "message", message: "Shot (target)", message_RU: "Выстрел (таргет)" }],
 		"s-456-1002-107-0": [{ type: "text", sub_type: "message", message: "Many Pokes", message_RU: "Серия ударов" }],
 		"s-456-1002-110-0": [{ type: "text", sub_type: "message", message: "Clap", message_RU: "Удар вперед" }],
+		"s-456-1002-212-0": [{ type: "text", sub_type: "message", message: "Jump Back", message_RU: "Прыжок назад" }],
 		"s-456-1002-3110-0": [{ type: "text", sub_type: "message", message: "Breath", message_RU: "Дыхание" }],
 		"s-456-1002-3113-0": [{ type: "text", sub_type: "message", message: "AOE", message_RU: "АОЕ", delay: 4000 }],
 
