233c0a856e79decce207973e1a84b797388caa4e
Dmitry Shin <dmitry.s@hsdn.org>
2021-02-25 01:05:38 +0400

    Update

--- guides/3206.js
+++ guides/3206.js
@@ -19,39 +19,46 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "func", func: () => combo_start = true },
 			{ type: "func", func: () => combo_start = false, delay: 4000 }
 		],
-		"s-3206-1000-105-0": [{ type: "text", sub_type: "message", message: "Knockback Spin", message_RU: "Оборот (откид)", check_func: () => combo_start === true }],
+		// "s-3206-1000-105-0": [{ type: "text", sub_type: "message", message: "Knockback Spin", message_RU: "Оборот (откид)", check_func: () => combo_start === true }],
 		"s-3206-1000-106-0": [{ type: "text", sub_type: "message", message: "Knockback Spin", message_RU: "Оборот (откид)" }],
+		"s-3206-1000-108-0": [{ type: "text", sub_type: "message", message: "Knockback Spin", message_RU: "Оборот (откид)" }],
 		"s-3206-1000-109-0": [{ type: "text", sub_type: "message", message: "Jump (Knockdown)", message_RU: "Прыжок (опрокид)" }],
-		"s-3206-1000-201-0": [{ type: "text", sub_type: "message", message: "Front Attack", message_RU: "Удар вперед" }],
-		"s-3206-1000-202-0": [{ type: "text", sub_type: "message", message: "Front Attack", message_RU: "Удар вперед" }],
-		"s-3206-1000-203-0": [{ type: "text", sub_type: "message", message: "Front Attack", message_RU: "Удар вперед" }],
+		"s-3206-1000-201-0": [{ type: "text", sub_type: "message", message: "Front", message_RU: "Удар вперед" }],
+		"s-3206-1000-202-0": [{ type: "text", sub_type: "message", message: "Front", message_RU: "Удар вперед" }],
+		"s-3206-1000-203-0": [{ type: "text", sub_type: "message", message: "Front + Wave", message_RU: "Удар вперед + волна" }],
 		"s-3206-1000-205-0": [{ type: "text", sub_type: "message", message: "Spin", message_RU: "Крутилка" }],
 		"s-3206-1000-206-0": [{ type: "text", sub_type: "message", message: "Spin", message_RU: "Крутилка" }],
 		"s-3206-1000-207-0": [{ type: "text", sub_type: "message", message: "Spin", message_RU: "Крутилка" }],
-		"s-3206-1000-209-0": [{ type: "text", sub_type: "message", message: "Stomp (Knockdown)", message_RU: "Опрокид" }],
-		"s-3206-1000-210-0": [{ type: "text", sub_type: "message", message: "Stomp (Knockdown)", message_RU: "Опрокид" }],
+		"s-3206-1000-209-0": [
+			{ type: "text", sub_type: "message", message: "Stomp (Knockdown)", message_RU: "Опрокид" },
+			{ type: "spawn", func: "circle", args: [true, 553, 0, 0, 10, 400, 0, 2500] }
+		],
+		"s-3206-1000-210-0": [
+			{ type: "text", sub_type: "message", message: "Stomp (Knockdown)", message_RU: "Опрокид" },
+			{ type: "spawn", func: "circle", args: [true, 553, 0, 0, 10, 400, 0, 2500] }
+		],
 		"s-3206-1000-211-0": [{ type: "text", sub_type: "message", message: "Push", message_RU: "Откид" }],
 		"s-3206-1000-216-0": [{ type: "text", sub_type: "message", message: "Somersault", message_RU: "Кувырок" }],
-
-		"s-3206-1000-508-0": [ // 516
+		"s-3206-1000-508-0": [
 			{ type: "text", sub_type: "message", message: "Waves Outward", message_RU: "Волны наружу" },
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 18, 157, 1500, 5000] },
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 307, 1500, 5000] }
 		],
-		"s-3206-1000-509-0": [ // 517
+		"s-3206-1000-509-0": [
 			{ type: "text", sub_type: "message", message: "Waves Inward", message_RU: "Волны внутрь" },
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 18, 157, 1500, 5000] },
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 307, 1500, 5000] }
 		],
-
 		"s-3206-1000-506-0": [{ type: "text", sub_type: "message", message: "Bait (Stun)", message_RU: "Байт (стан)" }],
 		"s-3206-1000-507-0": [{ type: "text", sub_type: "message", message: "Leash + Jump (Knockdown)", message_RU: "Притяжка + прыжок (опрокид)" }],
+		// "s-3206-1000-516-0": "s-3206-1000-508-0",
+		// "s-3206-1000-517-0": "s-3206-1000-509-0",
 
+		"s-3206-1000-518-0": [{ type: "text", sub_type: "message", message: "Unleash", message_RU: "Бешенство" }],
 		"s-3206-1000-519-0": [{ type: "text", sub_type: "message", message: "Unleash", message_RU: "Бешенство" }],
-		"s-3206-1000-310-0": [{ type: "text", sub_type: "message", message: "Front Attack", message_RU: "Удар вперед" }],
-		"s-3206-1000-311-0": [{ type: "text", sub_type: "message", message: "Front Attack", message_RU: "Удар вперед" }],
+		"s-3206-1000-307-0": [{ type: "text", sub_type: "message", message: "Spin", message_RU: "Крутилка" }],
+		"s-3206-1000-309-0": [{ type: "text", sub_type: "message", message: "Front Attack", message_RU: "Удар вперед" }],
 		"s-3206-1000-321-0": [{ type: "text", sub_type: "message", message: "AoE", message_RU: "АоЕ" }],
-		"s-3206-1000-324-0": [{ type: "text", sub_type: "message", message: "AoE", message_RU: "АоЕ" }],
-		"s-3206-1000-307-0": [{ type: "text", sub_type: "message", message: "Spin", message_RU: "Крутилка" }]
+		"s-3206-1000-324-0": [{ type: "text", sub_type: "message", message: "AoE", message_RU: "АоЕ" }]
 	};
 };
