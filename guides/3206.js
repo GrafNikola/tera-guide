35b49d9bb1a96720677462fbb2a8459615ac005c
Dmitry Shin <dmitry.s@hsdn.org>
2021-02-25 07:55:20 +0400

    Update

--- guides/3206.js
+++ guides/3206.js
@@ -4,8 +4,6 @@
 
 module.exports = (dispatch, handlers, guide, lang) => {
 
-	let combo_start = false;
-
 	return {
 		"nd-3206-1000": [
 			{ type: "stop_timers" },
@@ -15,17 +13,12 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"qb-3206-1000-32061001": [{ type: "text", sub_type: "message", message: "Debuff (Close)", message_RU: "Дебаф (ближние)" }],
 		"qb-3206-1000-32061002": [{ type: "text", sub_type: "message", message: "Debuff (Furthest)", message_RU: "Дебаф (дальние)" }],
 
-		"s-3206-1000-102-0": [
-			{ type: "func", func: () => combo_start = true },
-			{ type: "func", func: () => combo_start = false, delay: 4000 }
-		],
-		// "s-3206-1000-105-0": [{ type: "text", sub_type: "message", message: "Knockback Spin", message_RU: "Оборот (откид)", check_func: () => combo_start === true }],
 		"s-3206-1000-106-0": [{ type: "text", sub_type: "message", message: "Knockback Spin", message_RU: "Оборот (откид)" }],
 		"s-3206-1000-108-0": [{ type: "text", sub_type: "message", message: "Knockback Spin", message_RU: "Оборот (откид)" }],
 		"s-3206-1000-109-0": [{ type: "text", sub_type: "message", message: "Jump (Knockdown)", message_RU: "Прыжок (опрокид)" }],
 		"s-3206-1000-201-0": [{ type: "text", sub_type: "message", message: "Front", message_RU: "Удар вперед" }],
-		"s-3206-1000-202-0": [{ type: "text", sub_type: "message", message: "Front", message_RU: "Удар вперед" }],
-		"s-3206-1000-203-0": [{ type: "text", sub_type: "message", message: "Front + Wave", message_RU: "Удар вперед + волна" }],
+		"s-3206-1000-202-0": [{ type: "text", sub_type: "message", message: "Front AoE", message_RU: "Переднее АоЕ" }],
+		"s-3206-1000-203-0": [{ type: "text", sub_type: "message", message: "Front AoE + Wave", message_RU: "Переднее АоЕ + волна" }],
 		"s-3206-1000-205-0": [{ type: "text", sub_type: "message", message: "Spin", message_RU: "Крутилка" }],
 		"s-3206-1000-206-0": [{ type: "text", sub_type: "message", message: "Spin", message_RU: "Крутилка" }],
 		"s-3206-1000-207-0": [{ type: "text", sub_type: "message", message: "Spin", message_RU: "Крутилка" }],
@@ -40,19 +33,16 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-3206-1000-211-0": [{ type: "text", sub_type: "message", message: "Push", message_RU: "Откид" }],
 		"s-3206-1000-216-0": [{ type: "text", sub_type: "message", message: "Somersault", message_RU: "Кувырок" }],
 		"s-3206-1000-508-0": [
-			{ type: "text", sub_type: "message", message: "Waves Outward", message_RU: "Волны наружу" },
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 18, 157, 1500, 5000] },
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 307, 1500, 5000] }
+			{ type: "text", sub_type: "message", message: "Outward Waves (Out > In)", message_RU: "Волны наружу (от него > к нему)" },
+			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 18, 180, 1500, 5000] },
+			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 350, 1500, 5000] }
 		],
 		"s-3206-1000-509-0": [
-			{ type: "text", sub_type: "message", message: "Waves Inward", message_RU: "Волны внутрь" },
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 18, 157, 1500, 5000] },
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 307, 1500, 5000] }
+			{ type: "text", sub_type: "message", message: "Inward Waves (In > Out)", message_RU: "Волны внутрь (к нему > от него)" },
+			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 18, 180, 1500, 5000] },
+			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 350, 1500, 5000] }
 		],
-		// "s-3206-1000-506-0": [{ type: "text", sub_type: "message", message: "Bait (Stun)", message_RU: "Байт (стан)" }],
 		"s-3206-1000-507-0": [{ type: "text", sub_type: "message", message: "Leash + Jump (Knockdown)", message_RU: "Притяжка + прыжок (опрокид)" }],
-		// "s-3206-1000-516-0": "s-3206-1000-508-0",
-		// "s-3206-1000-517-0": "s-3206-1000-509-0",
 
 		"s-3206-1000-518-0": [{ type: "text", sub_type: "message", message: "Unleash", message_RU: "Бешенство" }],
 		"s-3206-1000-519-0": [{ type: "text", sub_type: "message", message: "Unleash", message_RU: "Бешенство" }],
