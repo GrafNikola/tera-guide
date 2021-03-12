b70a9fcb33f8e7639ba6531de9ae7f21b1c569ac
Dmitry Shin <dmitry.s@hsdn.org>
2021-03-12 20:26:42 +0400

    Update

--- guides/3206.js
+++ guides/3206.js
@@ -4,6 +4,8 @@
 
 module.exports = (dispatch, handlers, guide, lang) => {
 
+	let combo_start = false;
+
 	dispatch.hook("S_USER_EFFECT", 1, event => {
 		if (event.circle == 3 && event.operation == 1) {
 			handlers.marker({ id: event.target, color: "yellow", sub_delay: 1000000 });
@@ -15,7 +17,11 @@ module.exports = (dispatch, handlers, guide, lang) => {
 	return {
 		"nd-3206-1000": [
 			{ type: "stop_timers" },
-			{ type: "despawn_all" }
+			{ type: "despawn_all" },
+			{ type: "marker_remove_all" }
+		],
+		"h-3206-1000-99": [
+			{ type: "spawn", func: "marker", args: [false, 3, -700, 100, 60000000, false, ["Giant", "Giant Direction"]] }
 		],
 
 		"qb-3206-1000-32061001": [
@@ -27,8 +33,16 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "text", sub_type: "alert", message: "Soon to give stun...", message_RU: "Скоро давать стан...", delay: 2000 }
 		],
 
-		"s-3206-1000-106-0": [{ type: "text", sub_type: "message", message: "Knockback Spin", message_RU: "Оборот (откид)" }],
-		"s-3206-1000-108-0": [{ type: "text", sub_type: "message", message: "Knockback Spin", message_RU: "Оборот (откид)" }],
+		"s-3206-1000-102-0": [
+			{ type: "func", func: () => combo_start = true },
+			{ type: "func", func: () => combo_start = false, delay: 1400 }
+		],
+		"s-3206-1000-105-0": [{ type: "text", sub_type: "message", message: "Knockback Spin (Kaia)", message_RU: "Оборот (Кайа)", check_func: () => combo_start === true }],
+		"s-3206-1000-106-0": [
+			{ type: "text", sub_type: "message", message: "Knockback", message_RU: "Откид" },
+			{ type: "spawn", func: "circle", args: [true, 553, 0, 50, 10, 350, 0, 3000] }
+		],
+
 		"s-3206-1000-109-0": [
 			{ type: "text", sub_type: "message", message: "Jump (Knockdown)", message_RU: "Прыжок (опрокид)" },
 			{ type: "spawn", func: "circle", args: [true, 553, 0, 150, 10, 300, 0, 2500] }
@@ -41,14 +55,15 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-3206-1000-207-0": [{ type: "text", sub_type: "message", message: "Spin (Bleed)", message_RU: "Крутилка (кровоток)" }],
 		"s-3206-1000-209-0": [
 			{ type: "text", sub_type: "message", message: "Give Stun! (Knockdown)", message_RU: "Дать стан! (опрокид)" },
-			{ type: "spawn", func: "circle", args: [true, 553, 0, 0, 10, 400, 0, 1500] }
+			{ type: "spawn", func: "circle", args: [true, 553, 0, 50, 10, 400, 0, 1500] }
 		],
 		"s-3206-1000-210-0": [
 			{ type: "text", sub_type: "message", message: "Give Stun! (Knockdown)", message_RU: "Дать стан! (опрокид)" },
-			{ type: "spawn", func: "circle", args: [true, 553, 0, 0, 10, 400, 0, 1500] }
+			{ type: "spawn", func: "circle", args: [true, 553, 0, 50, 10, 400, 0, 1500] }
 		],
 		"s-3206-1000-211-0": [{ type: "text", sub_type: "message", message: "Push", message_RU: "Откид" }],
-		"s-3206-1000-216-0": [{ type: "text", sub_type: "message", message: "Somersault", message_RU: "Кувырок" }],
+		"s-3206-1000-212-0": [{ type: "text", sub_type: "message", message: "Somersault", message_RU: "Кувырок" }],
+		"s-3206-1000-215-0": [{ type: "text", sub_type: "message", message: "Somersault", message_RU: "Кувырок" }],
 		"s-3206-1000-508-0": [
 			{ type: "text", sub_type: "message", message: "Outward Waves (Out > In)", message_RU: "Волны наружу (от него > к нему)" },
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 18, 180, 1500, 5000] },
