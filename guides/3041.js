318e3a904bef0af21f60c2d504b60daf24d0c075
Dmitry Shin <dmitry.s@hsdn.org>
2021-05-10 23:10:09 +0400

    Update

--- guides/3041.js
+++ guides/3041.js
@@ -28,8 +28,8 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-3041-1000-125-0": [{ type: "text", sub_type: "message", message: "Hit | Frontal Stun", message_RU: "Удар | Передний стан" }],
 		"s-3041-1000-126-0": [
 			{ type: "text", sub_type: "message", message: "Frontal Stun", message_RU: "Передний стан" },
-			{ type: "spawn", func: "vector", args: [553, 35, 250, 10, 400, 0, 2000] },
-			{ type: "spawn", func: "vector", args: [553, 325, 250, 350, 400, 0, 2000] }
+			{ type: "spawn", func: "vector", args: [553, 60, 220, 20, 500, 0, 2000] },
+			{ type: "spawn", func: "vector", args: [553, 300, 220, 340, 500, 0, 2000] }
 		],
 		"s-3041-1000-129-0": [{ type: "text", sub_type: "message", message: "Somersault | Back Hit", message_RU: "Кувырок | Удар назад" }],
 		"s-3041-1000-130-0": [
@@ -67,8 +67,8 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-3041-2000-125-0": [{ type: "text", sub_type: "message", message: "Hit | Frontal Stun", message_RU: "Удар | Передний стан" }],
 		"s-3041-2000-126-0": [
 			{ type: "text", sub_type: "message", message: "Frontal Stun", message_RU: "Передний стан" },
-			{ type: "spawn", func: "vector", args: [553, 35, 250, 10, 400, 0, 2000] },
-			{ type: "spawn", func: "vector", args: [553, 325, 250, 350, 400, 0, 2000] }
+			{ type: "spawn", func: "vector", args: [553, 60, 220, 20, 500, 0, 2000] },
+			{ type: "spawn", func: "vector", args: [553, 300, 220, 340, 500, 0, 2000] }
 		],
 		"s-3041-2000-129-0": [{ type: "text", sub_type: "message", message: "Somersault | Back Hit", message_RU: "Кувырок | Удар назад" }],
 		"s-3041-2000-130-0": [
@@ -81,7 +81,9 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-3041-2000-144-0": [
 			{ type: "text", sub_type: "message", message: "Right X-Swipe", message_RU: "Черкаш справа (крест)" },
 			{ type: "spawn", func: "vector", args: [553, 305, 700, 130, 1200, 0, 3000] },
-			{ type: "spawn", func: "vector", args: [553, 325, 700, 130, 1200, 0, 3000] }
+			{ type: "spawn", func: "vector", args: [553, 325, 700, 130, 1200, 0, 3000] },
+			{ type: "spawn", func: "vector", args: [553, 30, 700, 230, 1200, 3000, 1700] },
+			{ type: "spawn", func: "vector", args: [553, 50, 700, 230, 1200, 3000, 1700] }
 		],
 		"s-3041-2000-145-0": [
 			{ type: "text", sub_type: "message", message: "Left X-Swipe", message_RU: "Черкаш слева (крест)" },
@@ -91,7 +93,9 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-3041-2000-146-0": [
 			{ type: "text", sub_type: "message", message: "Right Swipe", message_RU: "Черкаш справа" },
 			{ type: "spawn", func: "vector", args: [553, 355, 500, 180, 1000, 0, 3000] },
-			{ type: "spawn", func: "vector", args: [553, 25, 500, 180, 1000, 0, 3000] }
+			{ type: "spawn", func: "vector", args: [553, 25, 500, 180, 1000, 0, 3000] },
+			{ type: "spawn", func: "vector", args: [553, 335, 500, 180, 1000, 3000, 1700] },
+			{ type: "spawn", func: "vector", args: [553, 5, 500, 180, 1000, 3000, 1700] }
 		],
 		"s-3041-2000-147-0": [
 			{ type: "text", sub_type: "message", message: "Left Swipe", message_RU: "Черкаш слева" },
@@ -100,7 +104,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		],
 		"s-3041-2000-148-0": [
 			{ type: "text", sub_type: "message", message: "Stun (AoE)", message_RU: "Стан (АоЕ)" },
-			{ type: "spawn", func: "circle", args: [true, 553, 180, 50, null, 250, 0, 2000] }
+			{ type: "spawn", func: "circle", args: [true, 553, 180, 20, null, 250, 0, 2000] }
 		],
 		"s-3041-2000-151-0": [
 			{ type: "text", sub_type: "message", message: "Throws Combo | Stun (AoE)", message_RU: "Серия ударов | Стан (АоЕ)" },
@@ -108,7 +112,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		],
 		"s-3041-2000-152-0": [
 			{ type: "text", sub_type: "message", message: "Stun (AoE)", message_RU: "Стан (АоЕ)" },
-			{ type: "spawn", func: "circle", args: [true, 553, 0, 0, null, 250, 0, 2000] }
+			{ type: "spawn", func: "circle", args: [true, 553, 180, 20, null, 250, 0, 2000] }
 		],
 		"s-3041-2000-301-0": [{ type: "text", sub_type: "message", message: "Circles (Target)", message_RU: "Круги (таргет)" }],
 		"s-3041-2000-340-0": [
