8fe47bcbf3ef9d9ba60302f5eaaf0a3e205d3a85
Dmitry Shin <dmitry.s@hsdn.org>
2021-08-05 02:05:31 +0400

    Update

--- guides/3105.js
+++ guides/3105.js
@@ -20,15 +20,15 @@ module.exports = (dispatch, handlers, guide, lang) => {
 
 		"s-3105-1000-101-0": [{ type: "text", sub_type: "message", message: "Front Swing", message_RU: "Удар вперед" }],
 		"s-3105-1000-102-0": [
-			{ type: "spawn", func: "vector", args: [553, 0, 0, 180, 300, 2000, 2000] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 180, 200, 2000, 2000] },
 			{ type: "spawn", func: "vector", args: [553, 0, 0, 0, 300, 2000, 2000] },
 			{ type: "spawn", func: "vector", args: [553, 270, 150, 0, 300, 2000, 2000] },
-			{ type: "spawn", func: "vector", args: [553, 270, 150, 180, 300, 2000, 2000] },
+			{ type: "spawn", func: "vector", args: [553, 270, 150, 180, 200, 2000, 2000] },
 			{ type: "text", sub_type: "message", message: "Disc Throw", message_RU: "Диск" }
 		],
 		"s-3105-1000-104-0": [
 			{ type: "text", sub_type: "message", message: "Stun (AOE)", message_RU: "Стан (АОЕ)" },
-			{ type: "spawn", func: "circle", args: [true, 553, 0, 0, 12, 250, 100, 3000] }
+			{ type: "spawn", func: "circle", args: [true, 553, 0, 0, 12, 260, 100, 3000] }
 		],
 		"s-3105-1000-108-0": [
 			{ type: "text", sub_type: "message", message: "Push (Tank)", message_RU: "Откид (танк)" },
@@ -53,36 +53,37 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		],
 		"s-3105-1000-114-0": [
 			{ type: "text", sub_type: "message", message: "Back Laser", message_RU: "Задний лазер" },
-			{ type: "spawn", func: "vector", args: [553, 90, 60, 180, 500, 0, 2000] },
-			{ type: "spawn", func: "vector", args: [553, 270, 60, 180, 500, 0, 2000] }
+			{ type: "spawn", func: "vector", args: [553, 90, 60, 180, 500, 0, 3000] },
+			{ type: "spawn", func: "vector", args: [553, 270, 60, 180, 500, 0, 3000] }
 		],
 		"s-3105-1000-120-0": [{ type: "text", sub_type: "message", message: "Storm", message_RU: "Шторм" }],
 		"s-3105-1000-107-0": [
 			{ type: "text", sub_type: "message", message: "Spin (Kaia) | Back Laser", message_RU: "Крутилка (кайа) | Задний лазер" },
 			{ type: "spawn", func: "circle", args: [true, 553, 0, 0, 12, 260, 100, 5000] }
 		],
-		"s-3105-1000-303-0": [{ type: "text", sub_type: "message", message: "Kick (Push)", message_RU: "Удар ногой (откид)" }],
+		"s-3105-1000-117-0": [{ type: "text", sub_type: "message", message: "Kick", message_RU: "Удар ногой" }],
+		"s-3105-1000-118-0": [{ type: "text", sub_type: "message", message: "Kick (Dodge)", message_RU: "Удар ногой (Эвейд)" }],
 
 		// Donuts combo <80%
-		"qb-3105-1000-31051013": [{ type: "text", sub_type: "message", message: "Donuts: Out > In > Dodge", message_RU: "Бублики: От него > К нему > Эвейд" }],
-		"qb-3105-1000-31051014": [{ type: "text", sub_type: "message", message: "Donuts: In > Out > Dodge", message_RU: "Бублики: К нему > От него > Эвейд" }],
+		"qb-3105-1000-31051013": [{ type: "text", sub_type: "message", message: "Outward Donuts (Out > In)", message_RU: "Бублики наружу (от него > к нему)" }],
+		"qb-3105-1000-31051014": [{ type: "text", sub_type: "message", message: "Inward Donuts (In > Out)", message_RU: "Бублики внутрь (к нему > от него)" }],
 		"s-3105-1000-115-0": [
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 18, 180, 1500, 5000] },
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 360, 1500, 5000] },
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 10, 550, 1500, 5000] },
-			{ type: "text", sub_type: "message", delay: 4900, message: "Dodge", message_RU: "Эвейд" }
+			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 330, 1500, 5000] },
+			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 10, 510, 1500, 5000] },
+			{ type: "text", sub_type: "message", delay: 4800, message: "Dodge", message_RU: "Эвейд" }
 		],
 		"s-3105-1000-116-0": [
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 18, 180, 1500, 5000] },
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 360, 1500, 5000] },
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 10, 550, 1500, 5000] },
-			{ type: "text", sub_type: "message", delay: 4900, message: "Dodge", message_RU: "Эвейд" }
+			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 330, 1500, 5000] },
+			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 10, 510, 1500, 5000] },
+			{ type: "text", sub_type: "message", delay: 4800, message: "Dodge", message_RU: "Эвейд" }
 		],
 
 		"qb-3105-1000-31051002": [{ type: "text", sub_type: "message", message: "Shield!", message_RU: "Щит!" }], // <50%
 		"s-3105-1000-123-0": [{ type: "text", sub_type: "message", message: "Break Failure", message_RU: "Щит не пробит" }],
 
-		"qb-3105-1000-31051004": [{ type: "text", sub_type: "message", message: "Bait", message_RU: "Байт" }], // range check
+		"qb-3105-1000-31051004": [{ type: "text", sub_type: "message", message: "Bait (Lasers)", message_RU: "Байт (лазеры)" }], // range check
 		"qb-3105-1000-31051005": [{ type: "text", sub_type: "message", message: "Bait", message_RU: "Байт" }], // get stun
 		"qb-3105-1000-31051006": [{ type: "text", sub_type: "message", message: "Bait", message_RU: "Байт" }], // cast ress
 		"qb-3105-1000-31051007": [{ type: "text", sub_type: "message", message: "Puddles", message_RU: "Лужи" }], // <40%
@@ -92,12 +93,13 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "text", sub_type: "message", message: "LASERS + WAVE", message_RU: "ЛАЗЕРЫ + ВОЛНА" },
 			{ type: "text", sub_type: "notification", message: "LASERS + WAVE", message_RU: "ЛАЗЕРЫ + ВОЛНА", speech: false }
 		],
+		"s-3105-1000-310-0": [{ type: "spawn", func: "marker", args: [false, 0, 0, 0, 8000, true, ["Lasers", "Wave"]] }],
 		"s-3105-1000-304-0": [ // red
-			{ type: "text", sub_type: "message", message: "Dodge!", message_RU: "Эвейд!", delay: 200 },
+			{ type: "text", sub_type: "message", message: "Wave (dodge)", message_RU: "Волна (эвейд)", delay: 100 },
 			{ type: "text", sub_type: "message", message: "Give Stun!", message_RU: "Дать стан!", delay: 2000 }
 		],
 		"s-3105-1000-305-0": [ // blue
-			{ type: "text", sub_type: "message", message: "Dodge!", message_RU: "Эвейд!", delay: 200 },
+			{ type: "text", sub_type: "message", message: "Wave (dodge)", message_RU: "Волна (эвейд)", delay: 100 },
 			{ type: "text", sub_type: "message", message: "Give Stun!", message_RU: "Дать стан!", delay: 2000 }
 		],
 		"s-3105-1000-121-0": [{ type: "text", sub_type: "message", message: "AoE", message_RU: "АоЕ" }], // red
@@ -131,7 +133,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-3105-1005-102-0": "s-3105-1001-102-0",
 		"s-3105-1006-102-0": "s-3105-1001-102-0",
 
-		"s-3105-1001-103-0": [{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 3000, 1000, 4000] }], // laser (core)
+		"s-3105-1001-103-0": [{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 3000, 0, 5000] }], // laser (core)
 		"s-3105-1002-103-0": "s-3105-1001-103-0",
 		"s-3105-1003-103-0": "s-3105-1001-103-0",
 		"s-3105-1004-103-0": "s-3105-1001-103-0",
--- guides/3205.js
+++ guides/3205.js
@@ -21,15 +21,15 @@ module.exports = (dispatch, handlers, guide, lang) => {
 
 		"s-3205-1000-101-0": [{ type: "text", sub_type: "message", message: "Front Swing", message_RU: "Удар вперед" }],
 		"s-3205-1000-102-0": [
-			{ type: "spawn", func: "vector", args: [553, 0, 0, 180, 300, 2000, 2000] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 180, 200, 2000, 2000] },
 			{ type: "spawn", func: "vector", args: [553, 0, 0, 0, 300, 2000, 2000] },
 			{ type: "spawn", func: "vector", args: [553, 270, 150, 0, 300, 2000, 2000] },
-			{ type: "spawn", func: "vector", args: [553, 270, 150, 180, 300, 2000, 2000] },
+			{ type: "spawn", func: "vector", args: [553, 270, 150, 180, 200, 2000, 2000] },
 			{ type: "text", sub_type: "message", message: "Disc Throw", message_RU: "Диск" }
 		],
 		"s-3205-1000-104-0": [
 			{ type: "text", sub_type: "message", message: "Stun (AOE)", message_RU: "Стан (АОЕ)" },
-			{ type: "spawn", func: "circle", args: [true, 553, 0, 0, 12, 250, 100, 3000] }
+			{ type: "spawn", func: "circle", args: [true, 553, 0, 0, 12, 260, 100, 3000] }
 		],
 		"s-3205-1000-108-0": [
 			{ type: "text", sub_type: "message", message: "Push (Tank)", message_RU: "Откид (танк)" },
@@ -54,51 +54,53 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		],
 		"s-3205-1000-114-0": [
 			{ type: "text", sub_type: "message", message: "Back Laser", message_RU: "Задний лазер" },
-			{ type: "spawn", func: "vector", args: [553, 90, 60, 180, 500, 0, 2000] },
-			{ type: "spawn", func: "vector", args: [553, 270, 60, 180, 500, 0, 2000] }
+			{ type: "spawn", func: "vector", args: [553, 90, 60, 180, 500, 0, 3000] },
+			{ type: "spawn", func: "vector", args: [553, 270, 60, 180, 500, 0, 3000] }
 		],
 		"s-3205-1000-120-0": [{ type: "text", sub_type: "message", message: "Storm", message_RU: "Шторм" }],
 		"s-3205-1000-107-0": [
 			{ type: "text", sub_type: "message", message: "Spin (Kaia) | Back Laser", message_RU: "Крутилка (кайа) | Задний лазер" },
 			{ type: "spawn", func: "circle", args: [true, 553, 0, 0, 12, 260, 100, 5000] }
 		],
-		"s-3205-1000-303-0": [{ type: "text", sub_type: "message", message: "Kick (Push)", message_RU: "Удар ногой (откид)" }],
+		"s-3205-1000-117-0": [{ type: "text", sub_type: "message", message: "Kick", message_RU: "Удар ногой" }],
+		"s-3205-1000-118-0": [{ type: "text", sub_type: "message", message: "Kick (Dodge)", message_RU: "Удар ногой (Эвейд)" }],
 
 		// Donuts combo <80%
-		"qb-3205-1000-32051013": [{ type: "text", sub_type: "message", message: "Donuts: Out > In > Dodge", message_RU: "Бублики: От него > К нему > Эвейд" }],
-		"qb-3205-1000-32051014": [{ type: "text", sub_type: "message", message: "Donuts: In > Out > Dodge", message_RU: "Бублики: К нему > От него > Эвейд" }],
+		"qb-3205-1000-32051013": [{ type: "text", sub_type: "message", message: "Outward Donuts (Out > In)", message_RU: "Бублики наружу (от него > к нему)" }],
+		"qb-3205-1000-32051014": [{ type: "text", sub_type: "message", message: "Inward Donuts (In > Out)", message_RU: "Бублики внутрь (к нему > от него)" }],
 		"s-3205-1000-115-0": [
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 18, 180, 1500, 5000] },
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 360, 1500, 5000] },
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 10, 550, 1500, 5000] },
-			{ type: "text", sub_type: "message", delay: 4900, message: "Dodge", message_RU: "Эвейд" }
+			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 330, 1500, 5000] },
+			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 10, 510, 1500, 5000] },
+			{ type: "text", sub_type: "message", delay: 4800, message: "Dodge", message_RU: "Эвейд" }
 		],
 		"s-3205-1000-116-0": [
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 18, 180, 1500, 5000] },
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 360, 1500, 5000] },
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 10, 550, 1500, 5000] },
-			{ type: "text", sub_type: "message", delay: 4900, message: "Dodge", message_RU: "Эвейд" }
+			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 330, 1500, 5000] },
+			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 10, 510, 1500, 5000] },
+			{ type: "text", sub_type: "message", delay: 4800, message: "Dodge", message_RU: "Эвейд" }
 		],
 
 		"qb-3205-1000-32051002": [{ type: "text", sub_type: "message", message: "Shield!", message_RU: "Щит!" }], // <50%
 		"s-3205-1000-123-0": [{ type: "text", sub_type: "message", message: "Break Failure", message_RU: "Щит не пробит" }],
 
-		"qb-3205-1000-32051004": [{ type: "text", sub_type: "message", message: "Bait", message_RU: "Байт" }], // range check
+		"qb-3205-1000-32051004": [{ type: "text", sub_type: "message", message: "Bait (Lasers)", message_RU: "Байт (лазеры)" }], // range check
 		"qb-3205-1000-32051005": [{ type: "text", sub_type: "message", message: "Bait", message_RU: "Байт" }], // get stun
 		"qb-3205-1000-32051006": [{ type: "text", sub_type: "message", message: "Bait", message_RU: "Байт" }], // cast ress
-		"qb-3205-1000-32051007": [{ type: "text", sub_type: "message", message: "Puddles", message_RU: "Лужи" }], // <40%
+		"qb-3205-1000-32051007": [{ type: "text", sub_type: "message", message: "Puddles", message_RU: "Лужи" }], // <45%
 
-		// Core mech <35%
+		// Core mech <40%
 		"qb-3205-1000-32051010": [
 			{ type: "text", sub_type: "message", message: "LASERS + WAVE", message_RU: "ЛАЗЕРЫ + ВОЛНА" },
 			{ type: "text", sub_type: "notification", message: "LASERS + WAVE", message_RU: "ЛАЗЕРЫ + ВОЛНА", speech: false }
 		],
+		"s-3105-1000-310-0": [{ type: "spawn", func: "marker", args: [false, 0, 0, 0, 8000, true, ["Lasers", "Wave"]] }],
 		"s-3205-1000-304-0": [ // red
-			{ type: "text", sub_type: "message", message: "Dodge!", message_RU: "Эвейд!", delay: 200 },
+			{ type: "text", sub_type: "message", message: "Wave (dodge)", message_RU: "Волна (эвейд)", delay: 100 },
 			{ type: "text", sub_type: "message", message: "Give Stun!", message_RU: "Дать стан!", delay: 2000 }
 		],
 		"s-3205-1000-305-0": [ // blue
-			{ type: "text", sub_type: "message", message: "Dodge!", message_RU: "Эвейд!", delay: 200 },
+			{ type: "text", sub_type: "message", message: "Wave (dodge)", message_RU: "Волна (эвейд)", delay: 100 },
 			{ type: "text", sub_type: "message", message: "Plague of Exhaustion", message_RU: "Чума/регресс", class_position: "priest", delay: 1000 },
 			{ type: "text", sub_type: "message", message: "Regression", message_RU: "Регресс", class_position: "mystic", delay: 1000 },
 			{ type: "text", sub_type: "message", message: "Give Stun!", message_RU: "Дать стан!", delay: 2000 }
@@ -134,7 +136,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-3205-1005-102-0": "s-3205-1001-102-0",
 		"s-3205-1006-102-0": "s-3205-1001-102-0",
 
-		"s-3205-1001-103-0": [{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 3000, 1000, 4000] }], // laser (core)
+		"s-3205-1001-103-0": [{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 3000, 0, 5000] }], // laser (core)
 		"s-3205-1002-103-0": "s-3205-1001-103-0",
 		"s-3205-1003-103-0": "s-3205-1001-103-0",
 		"s-3205-1004-103-0": "s-3205-1001-103-0",
