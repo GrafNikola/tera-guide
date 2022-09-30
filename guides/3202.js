e80985c7ea358a2820d9aea0a264b3efe7265e7b
Dmitry S <dmitry.s@hsdn.org>
2022-09-30 04:24:37 +0400

    Update 3202

--- guides/3202.js
+++ guides/3202.js
@@ -108,7 +108,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		],
 		// Right Foot (outward waves)
 		"s-3202-1000-1121-0": [
-			{ type: "text", sub_type: "message", message: "Pizza", message_RU: "Пицца" },
+			{ type: "text", sub_type: "message", message: "Pizza | Outward Waves", message_RU: "Пицца | Волны наружу" },
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 8, 550, 0, 3000] },
 			{ type: "spawn", func: "vector", args: [912, 0, 50, 60, 450, 0, 4000] },
 			{ type: "spawn", func: "vector", args: [912, 0, 50, 120, 450, 0, 4000] },
@@ -117,7 +117,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "text", sub_type: "message", delay: 1700, message: "Dodge!", message_RU: "Эвейд!" }
 		],
 		"s-3202-1000-2121-0": [
-			{ type: "text", sub_type: "message", message: "Pizza", message_RU: "Пицца" },
+			{ type: "text", sub_type: "message", message: "Pizza | Outward Waves", message_RU: "Пицца | Волны наружу" },
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 8, 550, 0, 3000] },
 			{ type: "spawn", func: "vector", args: [912, 0, 50, 60, 450, 0, 4000] },
 			{ type: "spawn", func: "vector", args: [912, 0, 50, 120, 450, 0, 4000] },
@@ -151,7 +151,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		],
 		// Left Foot (inward waves)
 		"s-3202-1000-1124-0": [
-			{ type: "text", sub_type: "message", message: "Pizza", message_RU: "Пицца" },
+			{ type: "text", sub_type: "message", message: "Pizza | Inward Waves", message_RU: "Пицца | Волны внутрь" },
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 8, 550, 0, 3000] },
 			{ type: "spawn", func: "vector", args: [912, 0, 50, 60, 450, 0, 4000] },
 			{ type: "spawn", func: "vector", args: [912, 0, 50, 120, 450, 0, 4000] },
@@ -160,7 +160,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "text", sub_type: "message", delay: 1550, message: "Dodge!", message_RU: "Эвейд!" }
 		],
 		"s-3202-1000-2124-0": [
-			{ type: "text", sub_type: "message", message: "Pizza", message_RU: "Пицца" },
+			{ type: "text", sub_type: "message", message: "Pizza | Inward Waves", message_RU: "Пицца | Волны внутрь" },
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 8, 550, 0, 3000] },
 			{ type: "spawn", func: "vector", args: [912, 0, 50, 60, 450, 0, 4000] },
 			{ type: "spawn", func: "vector", args: [912, 0, 50, 120, 450, 0, 4000] },
