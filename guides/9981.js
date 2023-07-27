0cff47ac372685f5903d456944312d64536d689e
Dmitry S <dmitry.s@hsdn.org>
2023-07-28 00:01:40 +0400

    Update 9781, 9981

--- guides/9981.js
+++ guides/9981.js
@@ -12,6 +12,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			handlers.event([
 				{ type: "text", sub_type: "message", message: "Pizza", message_RU: "Пицца" },
 				{ type: "spawn", func: "marker", args: [false, one * 45 + 70, 500, 0, 5000, true, null] },
+				{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 8, 330, 0, 6000] },
 				{ type: "spawn", func: "marker", args: [false, two * 45 + 70, 300, 7000, 5000, true, null] }
 			]);
 		}
@@ -97,7 +98,10 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "semicircle", args: [180, 360, 912, 0, 0, 10, 300, 0, 1500] },
 			{ type: "spawn", func: "semicircle", args: [180, 360, 912, 0, 0, 8, 360, 0, 1500] }
 		],
-		"s-981-1000-1301-0": [{ type: "text", sub_type: "message", message: "AOE (Go to the safe)", message_RU: "АОЕ (отойти в сейф зону)" }],
+		"s-981-1000-1301-0": [
+			{ type: "text", sub_type: "message", message: "AOE", message_RU: "АОЕ" },
+			{ type: "text", sub_type: "message", message: "Dodge! (Go to the safe)", message_RU: "Эвейд! (отойти в сейф зону)", delay: 1000 }
+		],
 		"s-981-1000-1303-0": [{ type: "text", sub_type: "message", message: "Spin", message_RU: "Крутилка" }],
 		"s-981-1000-1304-0": [
 			{ type: "text", sub_type: "message", message: "Flying", message_RU: "Взлет" },
@@ -105,18 +109,27 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		],
 		"s-981-1000-1308-0": [{ type: "text", sub_type: "message", message: "OUT", message_RU: "Наружу" }],
 		"s-981-1000-1309-0": [{ type: "text", sub_type: "message", message: "IN", message_RU: "Внутрь" }],
-		"s-981-1000-1113-0": [{ type: "text", sub_type: "message", message: "Front + AoEs", message_RU: "Передняя | AOE" }],
+		"s-981-1000-1112-0": [{ type: "text", sub_type: "message", message_RU: "Рывок назад", message: "Back Move" }],
+		"s-981-1000-1113-0": [{ type: "text", sub_type: "message", message: "Front + AoEs", message_RU: "Передняя + AOE" }],
+		"s-981-1000-1114-0": [
+			{ type: "text", sub_type: "message", message_RU: "Таргет", message: "Target Attack" },
+			{ type: "spawn", func: "vector", args: [553, 90, 150, 0, 1300, 0, 2500] },
+			{ type: "spawn", func: "vector", args: [553, 90, 75, 0, 1300, 0, 2500] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 0, 1300, 0, 2500] },
+			{ type: "spawn", func: "vector", args: [553, 270, 75, 0, 1300, 0, 2500] },
+			{ type: "spawn", func: "vector", args: [553, 270, 150, 0, 1300, 0, 2500] }
+		],
+		"s-981-1000-1115-0": [{ type: "text", sub_type: "message", delay: 3200, message_RU: "Эвейд", message: "Dodge" }], // dodge circle
+		"s-981-1000-1117-0": [{ type: "text", sub_type: "message", delay: 5200, message_RU: "Эвейд", message: "Dodge" }], // dodge circles
+		"s-981-1000-2103-0": "s-981-1000-1103-0",
 		"s-981-1000-2111-0": "s-981-1000-1111-0",
-		"s-981-1000-2301-0": "s-981-1000-1301-0",
-		"s-981-1000-2401-0": "s-981-1000-1401-0",
-		"s-981-1000-2402-0": "s-981-1000-1402-0",
-		"s-981-1000-2303-0": "s-981-1000-1303-0",
-		"s-981-1000-2304-0": "s-981-1000-1304-0",
-		"s-981-1000-2308-0": "s-981-1000-1308-0",
-		"s-981-1000-2309-0": "s-981-1000-1309-0",
+		"s-981-1000-2112-0": "s-981-1000-1112-0",
 		"s-981-1000-2113-0": "s-981-1000-1113-0",
+		"s-981-1000-2114-0": "s-981-1000-1114-0",
+		"s-981-1000-2115-0": "s-981-1000-1115-0",
+		"s-981-1000-2117-0": "s-981-1000-1117-0",
 		"qb-981-1000-98103": [{ type: "text", sub_type: "message", message: "Lead circle to the stone", message_RU: "Отвести круг к пилону" }],
-		"qb-981-1000-78107": [{ type: "text", sub_type: "message", message: "Lead circles to the stone", message_RU: "Отвести круги к пилону" }],
+		"qb-981-1000-98106": [{ type: "text", sub_type: "message", message: "Lead circles to the stone", message_RU: "Отвести круги к пилону" }],
 
 		// 2 BOSS
 		"nd-981-2000": [
@@ -132,6 +145,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		],
 		"s-981-2000-1138-0": [ // T1
 			{ type: "event", delay: 6900, args: [
+				{ type: "text", sub_type: "notification", message: "Out > In > Side > Side", message_RU: "Наружу > Внутрь > В сторону > В сторону" },
 				// x6 normal + in circle
 				{ type: "spawn", func: "marker", args: [false, 15, 270, 0, 3000, true, null] },
 				{ type: "spawn", func: "marker", args: [false, 75, 270, 0, 3000, true, null] },
@@ -162,6 +176,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		],
 		"s-981-2000-1139-0": [ // T2
 			{ type: "event", delay: 7900, args: [
+				{ type: "text", sub_type: "notification", message: "Side > In > Out > Side", message_RU: "В сторону > Внутрь > Наружу > В сторону" },
 				// x6 reverse
 				{ type: "spawn", func: "marker", args: [false, 45, 270, 0, 1500, true, null] },
 				{ type: "spawn", func: "marker", args: [false, 105, 270, 0, 1500, true, null] },
@@ -199,6 +214,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		],
 		"s-981-2000-1140-0": [ // T1
 			{ type: "event", delay: 6900, args: [
+				{ type: "text", sub_type: "notification", message: "Out > In > Side > Side", message_RU: "Наружу > Внутрь > В сторону > В сторону" },
 				// in circle
 				{ type: "spawn", func: "marker", args: [false, 45, 270, 0, 1500, true, null] },
 				{ type: "spawn", func: "marker", args: [false, 105, 270, 0, 1500, true, null] },
@@ -229,6 +245,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		],
 		"s-981-2000-1141-0": [ // T2
 			{ type: "event", delay: 7900, args: [
+				{ type: "text", sub_type: "notification", message: "Out > Side > Side > In", message_RU: "Наружу > В сторону > В сторону > Внутрь" },
 				// x6 normal
 				{ type: "spawn", func: "marker", args: [false, 15, 270, 0, 1500, true, null] },
 				{ type: "spawn", func: "marker", args: [false, 75, 270, 0, 1500, true, null] },
@@ -261,14 +278,12 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "circle", args: [false, 553, 180, 340, 14, 270, 0, 2600] }
 		],
 		"s-981-2000-1108-0": [{ type: "text", sub_type: "message", message: "Front", message_RU: "Передний" }],
+		"s-981-2000-1110-0": [{ type: "text", sub_type: "message", message: "Back Move", message_RU: "Рыыок назад" }],
 		"s-981-2000-1111-0": [{ type: "text", sub_type: "message", message: "360 attack", message_RU: "Круговая" }],
-		"s-981-2000-1134-0": [
-			{ type: "text", sub_type: "message", message: "Combo Attack (Dodge)", message_RU: "Задний" },
-			{ type: "spawn", func: "circle", args: [true, 553, 0, 80, 8, 390, 0, 2000] }
-		],
-		"s-981-2000-1134-1": [
-			{ type: "spawn", func: "circle", args: [true, 553, 0, 75, 15, 175, 100, 2000] }
-		],
+		"s-981-2000-1114-0": [{ type: "text", sub_type: "message", message: "Pull", message_RU: "Притяжка" }],
+		"s-981-2000-1115-0": [{ type: "text", sub_type: "message", message: "Circles", message_RU: "Круги" }],
+		"s-981-2000-1115-1": [{ type: "text", sub_type: "message", message: "Dodge", message_RU: "Эвейд", delay: 150 }],
+		"s-981-2000-1117-0": [{ type: "text", sub_type: "message", message: "Jump", message_RU: "Прыжок" }],
 		"s-981-2000-1130-0": [
 			{ type: "text", sub_type: "message", message: "Left", message_RU: "Откид влево" },
 			{ type: "spawn", func: "vector", args: [553, 360, 400, 180, 800, 0, 2000] },
@@ -289,19 +304,39 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "semicircle", args: [0, 180, 912, 0, 0, 10, 300, 0, 1500] },
 			{ type: "spawn", func: "semicircle", args: [0, 180, 912, 0, 0, 8, 360, 0, 1500] }
 		],
+		"s-981-2000-1134-0": [
+			{ type: "text", sub_type: "message", message: "Inner + AoE", message_RU: "Ближний + АоЕ" },
+			{ type: "spawn", func: "circle", args: [true, 553, 0, 75, 14, 175, 0, 1500] }
+		],
+		"s-981-2000-1134-1": [
+			{ type: "spawn", func: "circle", args: [true, 553, 0, 80, 8, 390, 0, 2000] }
+		],
+		"s-981-2000-1136-0": [{ type: "text", sub_type: "message", message: "Donut", message_RU: "Бублик" }],
+		"s-981-2000-1202-0": [{ type: "text", sub_type: "message", message: "Target Throw", message_RU: "Бросок цели" }],
+		"s-981-2000-1205-0": [{ type: "text", sub_type: "message", message: "Target Throw", message_RU: "Бросок цели" }],
+		"s-981-2000-1206-0": [{ type: "text", sub_type: "message", message: "Pike (Target)", message_RU: "Копье (таргет)" }],
 		"s-981-2000-1302-0": [{ type: "text", sub_type: "message", message: "Bait (Target)", message_RU: "Байт (таргет)" }],
 		"s-981-2000-1302-1": [{ type: "text", sub_type: "message", message: "Dodge", message_RU: "Эвейд", delay: 1600 }],
-		"s-981-2000-2501-0": "s-981-2000-1501-0",
+		"s-981-2000-1502-0": [
+			{ type: "text", sub_type: "message", message: "AoE", message_RU: "АоЕ" },
+			{ type: "spawn", func: "circle", args: [true, 553, 0, 80, 8, 390, 0, 3000] }
+		],
+		"s-981-2000-1503-0": [{ type: "text", sub_type: "message", message: "Target Lockon", message_RU: "Захват цели" }],
+		"s-981-2000-1504-0": [{ type: "text", sub_type: "message", message: "Mobs Summon", message_RU: "Призыв мобов" }],
+		//
 		"s-981-2000-2106-0": "s-981-2000-1106-0",
 		"s-981-2000-2108-0": "s-981-2000-1108-0",
+		"s-981-2000-2110-0": "s-981-2000-1110-0",
 		"s-981-2000-2111-0": "s-981-2000-1111-0",
-		"s-981-2000-2134-0": "s-981-2000-1134-0",
-		"s-981-2000-2134-1": "s-981-2000-1134-1",
+		"s-981-2000-2114-0": "s-981-2000-1114-0",
+		"s-981-2000-2115-0": "s-981-2000-1115-0",
+		"s-981-2000-2115-1": "s-981-2000-1115-1",
+		"s-981-2000-2117-0": "s-981-2000-1117-0",
 		"s-981-2000-2130-0": "s-981-2000-1130-0",
 		"s-981-2000-2131-0": "s-981-2000-1131-0",
-		"s-981-2000-2302-0": "s-981-2000-1302-0",
-		"s-981-2000-2302-1": "s-981-2000-1302-1",
-		"qb-981-2000-468036": [{ type: "text", sub_type: "message", message: "Mobs Summon", message_RU: "Призыв мобов" }],
+		"s-981-2000-2134-0": "s-981-2000-1134-0",
+		"s-981-2000-2134-1": "s-981-2000-1134-1",
+		"s-981-2000-2136-0": "s-981-2000-1136-0",
 		// Pizza Mechanic
 		"s-981-927-1301-0": [{ type: "func", func: secondboss_floor_event, args: [4, 7] }],
 		"s-981-927-1302-0": [{ type: "func", func: secondboss_floor_event, args: [2, 6] }],
@@ -350,10 +385,12 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-981-3000-1113-0": [{ type: "text", sub_type: "message", message: "Bait", message_RU: "Байт" }],
 		"s-981-3000-1151-0": [{ type: "text", sub_type: "message", message: "Attention stun", message_RU: "Стан" }],
 		"s-981-3000-1152-0": [{ type: "text", sub_type: "message", message: "Stun + Back", message_RU: "Стан + Откид назад" }],
+		"s-981-3000-1152-1": [{ type: "text", sub_type: "message", message: "Dodge", message_RU: "Эвейд", delay: 1900 }],
 		"s-981-3000-1138-0": [{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 250, 0, 6000] }],
 		"s-981-3000-2113-0": "s-981-3000-1113-0",
 		"s-981-3000-2151-0": "s-981-3000-1151-0",
 		"s-981-3000-2152-0": "s-981-3000-1152-0",
+		"s-981-3000-2152-1": "s-981-3000-1152-1",
 		"s-981-3000-2138-0": "s-981-3000-1138-0"
 	};
 };
