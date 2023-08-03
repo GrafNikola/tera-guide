84eec3bb1f053beb86a35066981c5ae9a52100cd
Dmitry S <dmitry.s@hsdn.org>
2023-08-03 05:34:07 +0400

    Update 9780, 9980

--- guides/9980.js
+++ guides/9980.js
@@ -23,6 +23,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-980-1000-202-0": [{ type: "text", sub_type: "message", message: "Jump (Stun)", message_RU: "Прыжок (стан)" }],
 		"s-980-1000-108-0": [{ type: "text", sub_type: "message", message: "Dodge", message_RU: "Эвейд", delay: 2000 }],
 		"s-980-1000-302-0": [{ type: "text", sub_type: "message", message: "Spin", message_RU: "Крутилка" }],
+		"s-980-1000-302-1": [{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 1600, 0, 2000] }],
 		"s-980-1001-302-0": [{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 1600, 0, 4000] }],
 
 		// 2 boss
@@ -32,13 +33,13 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		],
 		"s-980-2000-105-0": [{ type: "text", sub_type: "message", message: "Heavy Front Attack", message_RU: "Мощный удар вперед" }],
 		"s-980-2000-106-0": [{ type: "text", sub_type: "message", message: "Flame Ray (Target)", message_RU: "Полоса (таргет)" }],
-		"s-980-2000-106-2": [{ type: "text", sub_type: "message", message: "Dodge", message_RU: "Эвейд" }],
+		"s-980-2000-106-1": [{ type: "text", sub_type: "message", message: "Dodge", message_RU: "Эвейд", delay: 500 }],
 		"s-980-2000-107-0": [{ type: "text", sub_type: "message", message: "Whip", message_RU: "Кнут" }],
 		"s-980-2000-107-1": [{ type: "text", sub_type: "message", message: "Dodge", message_RU: "Эвейд", delay: 1000 }],
 		"s-980-2000-108-0": [{ type: "text", sub_type: "message", message: "Front Attack (Stun)", message_RU: "Удар вперед (стан)" }],
 		"s-980-2000-301-0": [{ type: "text", sub_type: "message", message: "Jump (Target)", message_RU: "Прыжок (таргет)" }],
 		"s-980-2000-301-1": [{ type: "text", sub_type: "message", message: "Dodge", message_RU: "Эвейд", delay: 1000 }],
-		"s-980-2000-302-0": [{ type: "text", sub_type: "message", message: "AOE", message_RU: "АОЕ" }],
+		"s-980-2000-302-0": [{ type: "text", sub_type: "message", message: "AOE (Stand behind a stone)", message_RU: "АОЕ (встать за камень)" }],
 		"s-980-2000-303-0": [{ type: "text", sub_type: "message", message: "Circles", message_RU: "Круги" }],
 		"s-980-2000-304-0": [{ type: "text", sub_type: "message", message: "Jump", message_RU: "Прыжок" }],
 		"s-980-2000-304-1": [{ type: "text", sub_type: "message", message: "Dodge", message_RU: "Эвейд", delay: 1000 }],
@@ -54,14 +55,11 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-980-3000-109-0": [{ type: "text", sub_type: "message", message: "Double Front Attack", message_RU: "Двойной удар вперед" }],
 		"s-980-3000-110-0": [{ type: "text", sub_type: "message", message: "Wave Front", message_RU: "Волна вперед" }],
 		"s-980-3000-111-0": [{ type: "text", sub_type: "message", message: "Spin", message_RU: "Кувырок" }],
-		"s-980-3000-112-0": [{ type: "text", sub_type: "message", message: "Target", message_RU: "Таргет" }],
+		"s-980-3000-112-0": [{ type: "text", sub_type: "message", message: "Target (Front | Back)", message_RU: "Таргет (Передняя | Задняя)" }],
 		"s-980-3000-112-2": [{ type: "text", sub_type: "message", message: "Front | Back", message_RU: "Передняя | Задняя" }],
-		"s-980-3000-113-0": [{ type: "text", sub_type: "message", message: "Left Pushback", message_RU: "Левый откид назад" }],
-		"s-980-3000-114-0": [{ type: "text", sub_type: "message", message: "Right Pushback", message_RU: "Правый откид назад" }],
-		"s-980-3000-115-0": [
-			{ type: "text", sub_type: "message", message: "Charging", message_RU: "Зарядка" },
-			{ type: "text", sub_type: "message", message: "Dodge", message_RU: "Эвейд", delay: 13500 }
-		],
+		"s-980-3000-113-0": [{ type: "text", sub_type: "message", message: "Pushback", message_RU: "Откид назад" }],
+		"s-980-3000-114-0": "s-780-3000-113-0",
+		"s-980-3000-115-0": [{ type: "text", sub_type: "message", message: "Charging", message_RU: "Зарядка" }],
 		"s-980-3000-301-0": [{ type: "text", sub_type: "message", message: "Get Out | Get In", message_RU: "От него | К нему" }],
 		"s-980-3000-302-0": [{ type: "text", sub_type: "message", message: "Get In", message_RU: "К нему" }],
 		"s-980-3000-304-0": [{ type: "text", sub_type: "message", message: "Shot (Target)", message_RU: "Выстрел (таргет)" }],
@@ -74,7 +72,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-980-3000-308-0": "s-980-3000-306-0",
 		"s-980-3000-309-0": [
 			{ type: "text", sub_type: "message", message: "Jump", message_RU: "Прыжок" },
-			{ type: "text", sub_type: "message", message: "Dodge", message_RU: "Эвейд", delay: 2300 }
+			{ type: "text", sub_type: "message", message: "Dodge", message_RU: "Эвейд", delay: 2200 }
 		]
 	};
 };
