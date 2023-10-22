7a3a6fa9e1460d37cffb850d4a3ce3a0e54eeb50
Dmitry S <dmitry.s@hsdn.org>
2023-10-23 00:50:40 +0400

    Update 9983

--- guides/9983.js
+++ guides/9983.js
@@ -25,7 +25,8 @@ module.exports = (dispatch, handlers, guide, lang) => {
 				{ type: "spawn", func: "semicircle", args: [135, 405, 553, 0, 0, 20, 160, 0, 3000] },
 				{ type: "spawn", func: "semicircle", args: [135, 405, 553, 0, 0, 12, 220, 0, 3000] },
 				{ type: "spawn", func: "semicircle", args: [135, 405, 553, 0, 0, 10, 300, 0, 3000] },
-				{ type: "spawn", func: "semicircle", args: [135, 405, 553, 0, 0, 8, 360, 0, 3000] }
+				{ type: "spawn", func: "semicircle", args: [135, 405, 553, 0, 0, 8, 360, 0, 3000] },
+				{ type: "spawn", func: "semicircle", args: [135, 405, 553, 0, 0, 6, 420, 0, 3000] }
 			]);
 
 			first_skillid = 0;
@@ -36,7 +37,8 @@ module.exports = (dispatch, handlers, guide, lang) => {
 				{ type: "spawn", func: "semicircle", args: [-45, 225, 553, 0, 0, 20, 160, 0, 3000] },
 				{ type: "spawn", func: "semicircle", args: [-45, 225, 553, 0, 0, 12, 220, 0, 3000] },
 				{ type: "spawn", func: "semicircle", args: [-45, 225, 553, 0, 0, 10, 300, 0, 3000] },
-				{ type: "spawn", func: "semicircle", args: [-45, 225, 553, 0, 0, 8, 360, 0, 3000] }
+				{ type: "spawn", func: "semicircle", args: [-45, 225, 553, 0, 0, 8, 360, 0, 3000] },
+				{ type: "spawn", func: "semicircle", args: [-45, 225, 553, 0, 0, 6, 420, 0, 3000] }
 			]);
 
 			first_skillid = 0;
@@ -79,14 +81,21 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "despawn_all" }
 		],
 		"ns-983-1008": [{ type: "text", sub_type: "message", message: "Take Balls", message_RU: "Взять сферы" }],
-		"s-983-1000-101-0": [{ type: "text", sub_type: "message", message: "Stun", message_RU: "Кувырок (стан)", check_func: () => firstboss_prepare_stun }],
+		"s-983-1000-101-0": [
+			{ type: "text", sub_type: "message", message: "Stun", message_RU: "Кувырок (стан)", check_func: () => firstboss_prepare_stun },
+			{ type: "spawn", func: "circle", args: [true, 553, 0, 0, null, 470, 0, 3000], check_func: () => firstboss_prepare_stun }
+		],
 		"s-983-1000-102-0": [
 			{ type: "func", func: () => firstboss_prepare_stun = true },
 			{ type: "func", func: () => firstboss_prepare_stun = false, delay: 2700 }
 		],
 		"s-983-1000-104-0": [{ type: "text", sub_type: "message", message: "Jump", message_RU: "Прыжок" }],
 		"s-983-1000-109-0": [{ type: "text", sub_type: "message", message: "Push Back", message_RU: "Откид назад" }],
-		"s-983-1000-119-0": [{ type: "text", sub_type: "message", message: "Dodge (Heavy Attack)", message_RU: "Эвейд (тяжелый удар)", delay: 300 }],
+		"s-983-1000-119-0": [
+			{ type: "text", sub_type: "message", message: "Heavy", message_RU: "Удар" },
+			{ type: "text", sub_type: "message", message: "Dodge", message_RU: "Эвейд", delay: 250 },
+			{ type: "spawn", func: "circle", args: [true, 553, 0, 50, null, 350, 0, 3000] }
+		],
 		"s-983-1000-127-0": [{ type: "text", sub_type: "message", message: "Many Attack", message_RU: "Серия ударов" }],
 		"s-983-1000-128-0": [{ type: "text", sub_type: "message", message: "Lazer", message_RU: "Луч" }],
 		"s-983-1000-130-0": "s-983-1000-128-0",
@@ -135,7 +144,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-983-2000-105-0": [{ type: "text", sub_type: "message", message: "Whip (Dodge)", message_RU: "Хлыст (эвейд)" }],
 		"s-983-2000-110-0": [{ type: "text", sub_type: "message", message: "Front Attack (Dodge)", message_RU: "Удар вперед (эвейд)" }],
 		"s-983-2000-112-0": [{ type: "text", sub_type: "message", message: "Push Back (Right)", message_RU: "Откид назад правой" }],
-		"s-983-2000-114-0": [{ type: "text", sub_type: "message", message: "Ground Hit", message_RU: "Удар в землю" }],
+		"s-983-2000-114-0": [{ type: "text", sub_type: "message", message: "Ground Hit (Dodge)", message_RU: "Удар в землю (эвейд)" }],
 		"s-983-2000-115-0": [{ type: "text", sub_type: "message", message: "Push Back (Left)", message_RU: "Откид назад левой" }],
 		"s-983-2000-119-0": [{ type: "text", sub_type: "message", message: "Jump (Dodge)", message_RU: "Прыжок (эвейд)" }],
 		"s-983-2000-120-0": [{ type: "text", sub_type: "message", message: "Front Attack | Push Back", message_RU: "Удар вперед | Откид назад" }],
@@ -151,13 +160,19 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "circle", args: [true, 553, 0, -50, null, 700, 0, 7000] }
 		],
 		"s-983-2000-324-0": [{ type: "text", sub_type: "message", message: "Dodge (Pull)", message_RU: "Эвейд (притяжка)", delay: 2400 }],
+		//
+		"s-983-1011-308-0": [{ type: "spawn", func: "circle", args: [true, 553, 0, 0, null, 305, 0, 1500] }],
+		"s-983-1012-308-0": "s-983-1011-308-0",
+		"s-983-1013-308-0": "s-983-1011-308-0",
+		"s-983-1014-308-0": "s-983-1011-308-0",
+		"s-983-1015-308-0": "s-983-1011-308-0",
 
 		// 3 BOSS
 		"nd-983-3000": [
 			{ type: "stop_timers" },
 			{ type: "despawn_all" }
 		],
-		"s-983-3000-106-0": [{ type: "text", sub_type: "message", message: "Push", message_RU: "Откид" }],
+		"s-983-3000-106-0": [{ type: "text", sub_type: "message", message: "Push (Tank)", message_RU: "Откид (танк)" }],
 		"s-983-3000-110-0": [{ type: "text", sub_type: "message", message: "Throw Front (Stun)", message_RU: "Удар вперед (стан)" }],
 		"s-983-3000-113-0": [{ type: "text", sub_type: "message", message: "Push Back", message_RU: "Откид назад" }],
 		"s-983-3000-114-0": [{ type: "text", sub_type: "message", message: "Bait (Target)", message_RU: "Байт (таргет)" }],
@@ -179,10 +194,10 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "vector", args: [553, 90, 80, 0, 500, 0, 2000] },
 			{ type: "spawn", func: "vector", args: [553, 270, 80, 0, 500, 0, 2000] }
 		],
-		"s-983-3000-124-0": [{ type: "text", sub_type: "message", message: "AoE", message_RU: "АоЕ" }],
+		"s-983-3000-124-0": [{ type: "text", sub_type: "message", message: "AoE (Dodge)", message_RU: "АоЕ (эвейд)" }],
 		"s-983-3000-125-0": [{ type: "text", sub_type: "message", message: "Hit | Frontal Stun", message_RU: "Удар | Передний стан" }],
 		"s-983-3000-126-0": [
-			{ type: "text", sub_type: "message", message: "Frontal Stun", message_RU: "Передний стан" },
+			{ type: "text", sub_type: "message", message: "Frontal Stun | Back Hit", message_RU: "Передний стан | Удар назад" },
 			{ type: "spawn", func: "vector", args: [553, 60, 220, 20, 500, 0, 2000] },
 			{ type: "spawn", func: "vector", args: [553, 300, 220, 340, 500, 0, 2000] }
 		],
